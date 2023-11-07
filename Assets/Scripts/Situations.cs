61b8c1043c307ab21f26d9554b26c249c0b86634
Imaneokacha <127397518+Imaneokacha@users.noreply.github.com>
2023-11-07 10:27:50 +0100

    sir

---
+++ Assets/Scripts/Situations.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Situations : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void RetourMenu(){
+         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Situation1(){
+
+    }
+    public void Situation2(){
+
+    }
+    public void Situation3(){
+        
+    }
+}
