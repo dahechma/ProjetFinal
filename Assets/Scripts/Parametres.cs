795f3c33f7362db2a686da7a6d44af05875656b3
AIT BENMESSAOUD Amine <amine.ait-benmessaoud@imt-atlantique.net>
2023-11-07 09:22:43 +0100

    le projet

---
+++ Assets/Scripts/Parametres.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Parametres : MonoBehaviour
+{
+    public GameObject MenuParametre;
+    public Slider SliderLuminosite;
+    public Slider SliderSon;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void MainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void ChangeBrightness()
+    {
+        RenderSettings.ambientIntensity = SliderLuminosite.value; 
+    }
+    public void ChangeVolume()
+    {
+        AudioSource[] allAudioSources = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource audioSource in allAudioSources)
+        {
+            audioSource.volume = SliderSon.value; 
+        }
+    }
+}
